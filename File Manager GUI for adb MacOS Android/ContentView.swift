2dec94c36992d52a65e65ef247b5287333e8458f
alfonso <bsh0ugba@duck.com>
2025-08-04 11:42:21 +0700

    Auto-commit after successful build at 2025-08-04 11:42:21

---
+++ File Manager GUI for adb MacOS Android/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  File Manager GUI for adb MacOS Android
+//
+//  Created by Alfonso Fiore on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ File Manager GUI for adb MacOS Android/File_Manager_GUI_for_adb_MacOS_AndroidApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_Manager_GUI_for_adb_MacOS_AndroidApp.swift
+//  File Manager GUI for adb MacOS Android
+//
+//  Created by Alfonso Fiore on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct File_Manager_GUI_for_adb_MacOS_AndroidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
