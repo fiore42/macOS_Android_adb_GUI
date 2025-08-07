db7f927f8c241b37daf4fe61e1d768163d9b9c83
alfonso <bsh0ugba@duck.com>
2025-08-07 23:12:21 +0700

    Auto-commit after successful build at 2025-08-07 23:12:21

--- File Manager GUI for adb MacOS Android/ContentView.swift
+++ File Manager GUI for adb MacOS Android/ContentView.swift
@@ -204,11 +204,11 @@ struct ContentView: View {
                             file.isSelected.toggle()
                             if file.isSelected {
                                 selectedIDs.wrappedValue.insert(file.id)
-                                print("Added file to selection: \(file.id)")
+                                print("Count [\(selectedIDs.wrappedValue.count)]. Added file to selection: \(file.id)")
 
                             } else {
                                 selectedIDs.wrappedValue.remove(file.id)
-                                print("Removed file to selection: \(file.id)")
+                                print("Count [\(selectedIDs.wrappedValue.count)]. Removed file to selection: \(file.id)")
                             }
                             onFocusChange()  // Ensure tap on checkbox focuses pane
                         }
@@ -227,10 +227,10 @@ struct ContentView: View {
                     file.isSelected.toggle()
                     if file.isSelected {
                         selectedIDs.wrappedValue.insert(file.id)
-                        print("Added file to selection: \(file.id)")
+                        print("Count [\(selectedIDs.wrappedValue.count)]. Added file to selection: \(file.id)")
                     } else {
                         selectedIDs.wrappedValue.remove(file.id)
-                        print("Removed file to selection: \(file.id)")
+                        print("Count [\(selectedIDs.wrappedValue.count)]. Removed file to selection: \(file.id)")
                     }
                     onFocusChange()
                     
