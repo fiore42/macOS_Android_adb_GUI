606bb8e5a35bcfb892dfbee60499d61483793e20
alfonso <bsh0ugba@duck.com>
2025-08-09 12:32:57 +0700

    Auto-commit after successful build at 2025-08-09 12:32:57

--- File Manager GUI for adb MacOS Android/Utils.swift
+++ File Manager GUI for adb MacOS Android/Utils.swift
@@ -31,7 +31,7 @@ func getFileSize(direction: CopyDirection, path: String) -> Int64 {
         if let attrs = try? FileManager.default.attributesOfItem(atPath: path),
             let size = attrs[.size] as? Int64 {
             if errorVerbosity >= .debug {
-                print("getFileSize attrs \(attrs)")
+                print("getFileSize size \(attrs[.size])")
             }
             return size
         }
